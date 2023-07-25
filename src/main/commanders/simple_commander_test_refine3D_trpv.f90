f1d8eeb850f055e803613932d4413827930afe09
Hart <hartaw@fwl-c142339.ncifcrf.gov>
2023-07-25 14:22:33 -0400

    created commander exp_gate and trpv1

---
+++ src/main/commanders/simple_commander_test_refine3D_trpv.f90
@@ -0,0 +1,25 @@
+module simple_commander_test_trpv_refine3D
+    include 'simple_lib.f08'
+    use simple_cmdline,        only: cmdline
+    use simple_commander_base, only: commander_base
+
+    implicit none
+
+    public :: test_refine3D_trpv_commander
+
+    type, extends(commander_base) :: test_refine3D_trpv_commander
+        contains
+        procedure :: execute      => exec_test_trpv_refine3D
+    end type test_refine3D_trpv_commander
+
+    contains
+
+    subroutine exec_test_trpv_refine3D(self, cline)
+        class(test_refine3D_trpv_commander), intent(inout) :: self
+        class(cmdline),                 intent(inout) :: cline
+        print*, "trpv commander is set"
+    end subroutine exec_test_trpv_refine3D
+
+end module simple_commander_test_trpv_refine3D
+
+!simple_exec prg=test_trpv_refine3D
